2b1737ee2f5f72f8580d5b2176b7bf7452393a3c
zebirdman <benjamin.futterleib@gmail.com>
2018-05-07 06:40:27 +1000

    decoupled args package

--- dimager/args.go
+++ dimager/args.go
@@ -4,10 +4,6 @@ import (
 	"fmt"
 )
 
-const (
-	appName = "dimager"
-)
-
 type option struct {
 	Name    string   // how the argument will be passed ie '-v'
 	reqArg  bool     // will it require an argument?
@@ -17,31 +13,32 @@ type option struct {
 }
 
 var (
+	// application name
+	appName = "Your app"
 	// option none, used for when no arguments are passed
 	none = &option{"none", false, "", nil, false}
 	// storage for a bad option passed
 	invalid = &option{"", false, "", nil, false}
 	// triggers the help page
 	help = &option{"help", false, "", nil, false}
-
-	// option for specifying a docker host
-	host = &option{"h", true, "", []string{"-p"}, false}
-	// option for docker cert path when using tls
-	path = &option{"c", true, "", []string{"-h"}, false}
-	// option for specifying that an image tag prefix should be added
-	addP = &option{"p", true, "", nil, false}
-	// option to specify that existing image tag prefix will be replaced
-	repP = &option{"r", true, "", nil, false}
-	// option to specify that the existing image tags be removed after rename
-	clean = &option{"d", false, "", nil, false}
-	// option for verbose output from operation
-	verb = &option{"v", false, "", nil, false}
+	// array for holding cmd line cmd line options
+	cmdOptions = make([]*option, 0, 10)
+	// holds the help page string
+	appHelpPage = ""
 )
 
-// string array for cycling through arg checks
-var cmdOptions = [6]*option{host, path, addP, repP, clean, verb}
+func newOption(n string, r bool, c []string) *option {
+	no := &option{n, r, "", c, false}
+	cmdOptions = append(cmdOptions, no)
+	return no
+}
 
-func needsHelp(a []string) bool {
+func initArgs(n, h string) {
+	appHelpPage = h
+	appName = n
+}
+
+func needHelp(a []string) bool {
 	if len(a) > 1 {
 		for _, value := range a {
 			if value == "--help" {
@@ -54,12 +51,11 @@ func needsHelp(a []string) bool {
 }
 
 func checkArgs(allArgs []string) (*option, error) {
-	if !needsHelp(allArgs) {
+	if !needHelp(allArgs) && len(allArgs) > 1 {
 		a := allArgs[1:]
 		for i := 0; i < len(a); i++ {
 			argbytes := []byte(a[i])
 			for x := 0; x < len(argbytes); x++ {
-				//fmt.Print(argbytes)
 				if len(argbytes) == 1 {
 					if argbytes[0] == '-' {
 						invalid.Operand = string(argbytes[0])
@@ -75,6 +71,7 @@ func checkArgs(allArgs []string) (*option, error) {
 				if err != nil {
 					return invalid, fmt.Errorf("unknown command")
 				}
+				// if argument i required check that it is valid
 				if valOpt.reqArg {
 					if x+1 != len(argbytes) {
 						valOpt.Operand = string(argbytes[x+1])
@@ -92,8 +89,20 @@ func checkArgs(allArgs []string) (*option, error) {
 				}
 			}
 		}
+		return nil, nil
 	}
-	return none, nil
+	return none, fmt.Errorf("missing arguments")
+}
+
+func checkCommand(b byte) (*option, error) {
+	for _, cmd := range cmdOptions {
+		if string(b) == cmd.Name {
+			cmd.Enabled = true
+			return cmd, nil
+		}
+	}
+	invalid.Operand = string(b)
+	return invalid, fmt.Errorf("invalid command")
 }
 
 func showErrors(o *option, e error) {
@@ -101,6 +110,10 @@ func showErrors(o *option, e error) {
 		helpPage()
 	} else {
 		switch e.Error() {
+		case "missing arguments":
+			fmt.Printf("%s: %s\n", appName, e)
+			fmt.Printf("Try '%s --help' for more information\n", appName)
+			break
 		case "invalid option":
 			fmt.Printf("%s: %s '%s'\n", appName, e, o.Operand)
 			fmt.Printf("Try '%s --help' for more information\n", appName)
@@ -125,34 +138,6 @@ func showErrors(o *option, e error) {
 	}
 }
 
-func checkCommand(b byte) (*option, error) {
-	for _, cmd := range cmdOptions {
-		if string(b) == cmd.Name {
-			cmd.Enabled = true
-			return cmd, nil
-		}
-	}
-	invalid.Operand = string(b)
-	return invalid, fmt.Errorf("invalid command")
-}
-
 func helpPage() {
-	fmt.Printf(`dimager: allows for the easy renaming of docker image tags prefix's
-  usefull if we want to retag images to use with a pivate registry
-  author: Ben Futterleib
-
-Usage: dimager [OPTION]... [-s] SCRIPT_NAME (1st form)
-  or: dimager [OPTION]... [-h] DOCKER_HOST [-p] DOCKER_CERT_PATH (2nd form)
-In the first form specify a path to a script containing exports for the
-  DOCKER_HOST, DOCKER_CERT_PATH, DOCKER_TLS_VERIFY
-environment variables, must be valid executable script
-In the second form pass the DOCKER_HOST and DOCKER_CERT_PATH values using
-  the given flags, DOCKER_TLS_VERIFY will be set automatically
-
-Arguments:
-  -h 	specify the connection for the docker host
-  -p 	specify the path to the directory holding the client certs for docker
-  -s 	give a path to a script containing the relevant export env variables
-  --help 	display this help and exit
-`)
+	fmt.Printf(appHelpPage)
 }
--- dimager/dimager.go
+++ dimager/dimager.go
@@ -2,28 +2,86 @@ package main
 
 import (
 	"context"
+	"fmt"
 	"os"
 
 	"github.com/docker/docker/api/types"
 	"github.com/docker/docker/client"
 )
 
-var exMes = "program finished"
+const (
+	name = "dimager"
+)
 
+// save current env variables for restoration at end if needed
+var (
+	dockHost     = os.Getenv("DOCKER_HOST")
+	dockCertPath = os.Getenv("DOCKER_CERT_PATH")
+	dockTLS      = os.Getenv("DOCKER_TLS_VERIFY")
+	newPrefix    string
+	oldPrefix    string
+
+	// option for specifying a docker host
+	host = newOption("h", true, []string{"-p"})
+	// option for docker cert path when using tls
+	path = newOption("c", true, []string{"-h"})
+	// option for specifying the image prefix to add
+	addP = newOption("p", true, nil)
+	// option to specify that existing image tag prefix will be replaced
+	repP = newOption("r", true, nil)
+	// option to specify that the existing image tags be removed after rename
+	clean = newOption("d", false, nil)
+	// option for verbose output from operation
+	verb = newOption("v", false, nil)
+
+	hp = `dimager: allows for the easy renaming of docker image tags prefix's
+  usefull if we want to retag images to use with a pivate registry
+  author: Ben Futterleib
+
+Usage: dimager [OPTION]... [-s] SCRIPT_NAME (1st form)
+  or: dimager [OPTION]... [-h] DOCKER_HOST [-p] DOCKER_CERT_PATH (2nd form)
+In the first form specify a path to a script containing exports for the
+  DOCKER_HOST, DOCKER_CERT_PATH, DOCKER_TLS_VERIFY
+environment variables, must be valid executable script
+In the second form pass the DOCKER_HOST and DOCKER_CERT_PATH values using
+  the given flags, DOCKER_TLS_VERIFY will be set automatically
+
+Arguments:
+  -h 	specify the connection for the docker host
+  -p 	specify the path to the directory holding the client certs for docker
+  -s 	give a path to a script containing the relevant export env variables
+  --help 	display this help and exit
+`
+)
+
+func assignVariables() {
+	os.Setenv("DOCKER_HOST", host.Operand)
+	os.Setenv("DOCKER_CERT_PATH", path.Operand)
+	newPrefix = addP.Operand
+
+}
 func main() {
+	initArgs(name, hp)
 	op, err := checkArgs(os.Args)
 	if err != nil || help.Enabled {
 		showErrors(op, err)
 		os.Exit(1)
 	}
-
 	// initialize context and a new client
 	ctx := context.Background()
 	cli, err := client.NewEnvClient()
 	if err != nil {
 		panic(err)
 	}
+	// show enabled options and Arguments
+	for _, opt := range cmdOptions {
+		fmt.Printf("Enabled arguments:\n")
+		if opt.Enabled {
+			fmt.Printf("Name: %s  Operand: %s\n", opt.Name, opt.Operand)
+		}
+	}
 
+	// get images list
 	images, err := cli.ImageList(context.Background(), types.ImageListOptions{})
 	if err != nil {
 		panic(err)
